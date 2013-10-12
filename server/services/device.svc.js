da70280dd1b9549bdc11fe1384ae6a0e1f6e2a86
logmein3546 <emailme3546@gmail.com>
2013-10-12 19:52:49 -0400

    separating user and device api methods

--- server/services/device.svc.js
+++ server/services/device.svc.js
@@ -4,24 +4,11 @@ var DB_URL = 'mongodb://pooter.sandile.me:27017/data';
 var SCHEMA = 'data';
 
 module.exports.bind = function (app) {
-	app.get("/user/:id", function(req, res){
+	app.post("/user/device/new", function(req, res){
 		MongoClient.connect(DB_URL, function(err, db) {
 			if(err) throw err;
 			var collection = db.collection(SCHEMA);
-			collection.findOne({googleId : req.param("id")}, function(err, docs) {
-				if(err) throw err;
-				console.log(docs);
-				res.send(200, JSON.stringify(docs));
-				db.close();
-		 });
-	  })
-	});
-
-	app.post("/user/new", function(req, res){
-		MongoClient.connect(DB_URL, function(err, db) {
-			if(err) throw err;
-			var collection = db.collection(SCHEMA);
-			collection.save({googleId : req.body.googleId, devices : []}, function(err, docs) {
+			collection.update({googleId : req.body.googleId}, {$push: {devices: {chromeInstanceId: req.body.chromeInstanceId, name: req.body.name}}}, function(err, docs) {
 				if(err) throw err;
 				console.log(docs);
 				res.send(200, JSON.stringify(docs));
@@ -30,12 +17,11 @@ module.exports.bind = function (app) {
 	  })
 	});
 	
-	//debuging only
-	app.delete("/user/deleteAll", function(req, res){
+	app.delete("/user/device/remove", function(req, res){
 		MongoClient.connect(DB_URL, function(err, db) {
 			if(err) throw err;
 			var collection = db.collection(SCHEMA);
-			collection.remove(function(err, docs) {
+			collection.update({googleId : req.body.googleId}, {$pull: {devices: {chromeInstanceId: req.body.chromeInstanceId}}}, function(err, docs) {
 				if(err) throw err;
 				console.log(docs);
 				res.send(200, JSON.stringify(docs));
